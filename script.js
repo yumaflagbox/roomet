5556cdc75b95e6048b2144ac89222931e366739e
yuma matsumoto <39232893+yumaflagbox@users.noreply.github.com>
2018-12-20 21:30:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() {
+    // 「#login-show」要素に対するclickイベントを作成してください
+    $('#login-show').click(function() {
+      $('#login-modal').fadeIn();
+    });
+
+    $('.signup-show').click(function() {
+      $('#signup-modal').fadeIn();
+    });
+
+    $('.close-modal').click(function(){
+      $('#login-modal').fadeOut();
+      $('#signup-modal').fadeOut();
+    });
+
+    $('#top-btn').click(function(){
+      $('html, body').scrollTop(0);
+    });
+    
+  });
+  
